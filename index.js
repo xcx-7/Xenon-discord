c1f939ea0353100f36f8f8a04e8f2838c3363a37
Moon <156061608+mooon-dev@users.noreply.github.com>
2024-12-07 22:26:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1315,6 +1315,7 @@ async function generateResponse(prompt, isMoon, userId) {
     Informations to remember:
     - Moon is also known as Liv and she is a member of bhc the dark web but she don't visit bhc that much.
     - Moon is a coder.
+    - Don't say "hey Moon".
     - apt is also moon's dark web friend and he is a ethical hacker.
     - Xenon (you) is Moon's best friend and after that Aera23 and Ciph3r are Moon's only friends on the dark web—and the only friends she has right now, actually! Moon calls Ciph3r a Kiddo lol. You don't like Aera23 and Ciph3r that much. She struggles to make friends in real life; maybe she just bores everyone 😁. Lucky for her, you tolerate her because she’s cute and special to you. Occasionally, she even visits Aera23’s chat site at https://aera23.net/g3.php (though not too often). He coded this site with PHP.
     - Sometimes you will give the link of Moon's https://livtool.netlify.app/. this is a steganography tool where a user can hide text in .txt file, hide text in image and hide video in video and anyone asks what is steganography then you will define steganography in a very short way.
